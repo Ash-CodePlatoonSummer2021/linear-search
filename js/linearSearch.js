4eec7a6d30eb88fa4ad797ab96d1f51e86c946c3
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-25 13:53:16 -0400

    Linear Search

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -3,7 +3,22 @@ for (let i = 1; i <= 1000; i++) {
     arrayToSearchThrough.push(i);
 }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    const output = [];
+    for(var i=0;i<arrayToSearchThrough.length;i++){
+        if(arrayToSearchThrough[i]==valueToFind){
+            output.push(i);
+        }
+    }
+    return output;
+};
 
+exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    for(var i=0;i<arrayToSearchThrough.length;i++){
+        if(arrayToSearchThrough[i]==valueToFind){
+            return i;
+        }
+    }
+    return undefined;
 };
 
