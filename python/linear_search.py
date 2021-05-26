0edcef32ecffe3ea9c87ba363da3db8236dcad27
asriniv0 <ashsrinivasan1211@gmail.com>
2021-05-26 16:04:34 -0400

    python

--- python/linear_search.py
+++ python/linear_search.py
@@ -4,3 +4,7 @@ for number in range(1, 1001):
 
 def linear_search(value_to_find, array_to_search_through):
     # your code here
+    for x in range(len(array_to_search_through)):
+        if(array_to_search_through[x]==value_to_find):
+            return x
+    return None
